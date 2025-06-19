01d01582f6c7442c4ba7d6f37734719d58b1ba41
YunHeeSeong <P1953@office365.blue>
2025-06-19 12:01:56 +0900

    테스트소스

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,31 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async Task<string> GetLatestVersionFromGitHub() //버전확인
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://raw.githubusercontent.com/사용자명/저장소명/브랜치명/version.txt";
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string version = await response.Content.ReadAsStringAsync();
+                    return version.Trim();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("버전 확인 실패: " + ex.Message);
+                    return null;
+                }
+            }
         }
     }
 }
