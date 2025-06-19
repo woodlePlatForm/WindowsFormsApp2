13776d928a3944b7b3cbbd3ee8f758c0a6f6541a
YunHeeSeong <P1953@office365.blue>
2025-06-19 13:14:21 +0900

    ㄹㄹ

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net.Http;
@@ -20,15 +21,15 @@ namespace WindowsFormsApp2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string currentVersion = Application.ProductVersion;
-
+            
+            CheckVersionAndUpdate();
         }
 
         private async Task<string> GetLatestVersionFromGitHub() //버전확인
         {
             using (HttpClient client = new HttpClient())
             {
-                string url = "https://raw.githubusercontent.com/사용자명/저장소명/브랜치명/version.txt";
+                string url = "https://raw.githubusercontent.com/woodlePlatForm/WindowsFormsApp2/Test/version.txt"; //테스트용 버전관리프로그램
                 try
                 {
                     HttpResponseMessage response = await client.GetAsync(url);
@@ -43,5 +44,38 @@ namespace WindowsFormsApp2
                 }
             }
         }
+
+        private async void CheckVersionAndUpdate()
+        {
+            string latestVersion = await GetLatestVersionFromGitHub(); // 위에서 만든 메서드
+            string currentVersion = Application.ProductVersion;
+
+            if (latestVersion == null)
+                return;
+
+            if (latestVersion != currentVersion)
+            {
+                DialogResult result = MessageBox.Show(
+                    $"현재 버전({currentVersion})보다 최신 버전({latestVersion})이 존재합니다.\n업데이트 페이지로 이동할까요?",
+                    "업데이트 안내",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    System.Diagnostics.Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "https://github.com/woodlePlatForm/WindowsFormsApp2/releases/latest",
+                        UseShellExecute = true
+                    });
+                }
+
+                Application.Exit(); // 실행 차단
+            }
+        }
+
+
+
+        
     }
 }
